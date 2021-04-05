b7de84ad2a4cecddcd68cf5f6fbceeccaacfd806
Cole Kubossek <cole.kubossek@gmail.com>
2021-04-05 16:42:22 +0000

    changes ids for categories

--- routes/api.js
+++ routes/api.js
@@ -9,9 +9,9 @@ const  findCategory  = function(data){
   if (data.displayLink === "www.imdb.com") {
     category = 1;//categories of movie
   } else if(data.displayLink === "www.amazon.ca") {
-    category =2;//products
+    category = 2;//products
   }else if(data.displayLink === "www.yelp.ca") {
-    category =3 ;//food
+    category = 3 ;//food
   } else {
     category = 4 //to read;
   }
@@ -31,8 +31,4 @@ request(`https://www.googleapis.com/customsearch/v1?key=${key}&cx=${cx}&q=${quer
   console.log(data.items[1]);
   findCategory(data.items[1]);
 
-<<<<<<< HEAD
-=======
-
->>>>>>> be741731d7053c071aba51e13435f1097317a456
 });
